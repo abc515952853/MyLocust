77664f662ff7aa6bd81d5f8294cb95c3d764b52c
abc515952853 <123456789@qq.com>
2018-12-26 16:36:01 +0800

    千客通创建客户demo

--- MyLocustTest.py
+++ MyLocustTest.py
@@ -1,36 +1,33 @@
 from locust import HttpLocust, TaskSet, task
 import time
+import  json  
 
 
 class login(TaskSet):
     def on_start(self):
-        self.a = 1
+        payload = {"Phone":'18506826613',"CodeType":1,"Domain":'sss'}
+        headers = {"Content-Type":"application/json"}
+        self.client.post('api/SMS/Send/Code',data = json.dumps(payload),headers = headers)
+
+        payload = {"grant_type":'phonecode', "phone": '18506826613',"code":1234}
+        r = self.client.post('api/Token', data = payload)
+        self.session = r.json()["token_type"]+" "+r.json()["access_token"]
 
     def on_stop(self):
-        print('aaaaaaaaaaaaaaaa')
+        """
+        :return:
+        """
     
-    def getphone(self):
-        print('1850368265')
-
     @task(2)
-    def index1(self):
-        self.client.get('/')
-        print(self.a)
-        print(self.locust.b)
-        self.a = self.a+1
+    def addclient(self):
+        headers = {'Content-Type': "application/json",'Authorization':self.session,"Origin":'http://test.qkt.qianjifang.com.cn'}
+        payload = {"display": "测试客户","phone": "18506826613","level":'' }
+        r = self.client.post('api/Client', headers = headers,data = json.dumps(payload))
+        print(r.status_code,r.json())
 
-    @task(1)
-    def index2(self):
-        self.client.get('/')
-        print('2222222222222')
-
-  
 class BestTestIndexUser(HttpLocust):
-    host = "https://wwww.baidu.com" 
-    #这个类继承了HttpLocust，代表每个并发里面的每个用户
-    b ='111'
-    print('hahahah')
-    task_set = login #这个是每个用户都去干什么，指定了BestTest这个类，它就会每个用户去运行besttest这个类里面的方法
+    host = "http://api.qkt.qianjifang.com.cn/" 
+    task_set = login 
 
     min_wait = 0
     max_wait = 0
