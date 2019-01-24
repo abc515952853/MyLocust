d96676093473d959506d82697e4d1cade7471896
abc515952853 <123456789@qq.com>
2019-01-24 18:21:05 +0800

    111

--- LocustForMp.py
+++ LocustForMp.py
@@ -16,115 +16,211 @@ class MyTest(TaskSequence):
         self.userid = ''
         self.yonghu = 1
         self.ncdw = 1
+        self.activityphontophontoid = ''
         #登录
         self.LogIn()
 
-    ##########################################################基础接口###########################################################
-    #验证验证码
-    @task(1)
-    def test_CheckCaptcha(self):
-        self.CheckCaptcha()
+    # ##########################################################基础接口###########################################################
+    # #验证验证码
+    # @task(1)
+    # def test_CheckCaptcha(self):
+    #     self.CheckCaptcha()
 
-    #获取基础数据    
-    @task(1)
-    def test_BaseData(self):
-        self.BaseData()
-
-
-    #慢拍号列表
-    @task(1)
-    def test_Number(self):
-        self.Number()
-
-    #获取用户基本信息
-    @task(1)
-    def test_UserInfo(self):
-        self.UserInfo()
-
-    #系统礼物数据接口
-    @task(1)
-    def test_Gift(self):
-        self.Gift()
-
-    #版本检测接口
-    @task(1)
-    def test_CheckAccess(self):
-        self.CheckAccess()
-
-    #查询闪新闻-慢动态的消息数据
-    @task(1)
-    def test_Message(self):
-        self.Message()
+    # #获取基础数据    
+    # @task(1)
+    # def test_BaseData(self):
+    #     self.BaseData()
 
 
-    #删除消息数据（红点/黄点）
-    @task(1)
-    def test_CancelMessage(self):
-        self.CancelMessage()
+    # #慢拍号列表
+    # @task(1)
+    # def test_Number(self):
+    #     self.Number()
 
-    ##########################################################健康检测###########################################################
-    #添加成员
-    @seq_task(1)
-    @task(1)
-    def test_AddUser(self):
-        self.AddUser()
+    # #获取用户基本信息
+    # @task(1)
+    # def test_UserInfo(self):
+    #     self.UserInfo()
 
-    #选择成员列表
+    # #系统礼物数据接口
+    # @task(1)
+    # def test_Gift(self):
+    #     self.Gift()
+
+    # #版本检测接口
+    # @task(1)
+    # def test_CheckAccess(self):
+    #     self.CheckAccess()
+
+    # #查询闪新闻-慢动态的消息数据
+    # @task(1)
+    # def test_Message(self):
+    #     self.Message()
+
+
+    # #删除消息数据（红点/黄点）
+    # @task(1)
+    # def test_CancelMessage(self):
+    #     self.CancelMessage()
+
+    # ##########################################################健康检测###########################################################
+    # #添加成员
+    # @seq_task(1)
+    # @task(1)
+    # def test_AddUser(self):
+    #     self.AddUser()
+
+    # #选择成员列表
+    # @seq_task(2)
+    # @task(1)
+    # def test_UserList(self):
+    #     self.UserList()
+
+
+    # #同步检测报告
+    # @seq_task(3)
+    # @task(1)
+    # def test_SyncReport(self):
+    #     self.SyncReport()
+
+    # #报告列表
+    # @seq_task(4)
+    # @task(1)
+    # def test_ReportList(self):
+    #     self.ReportList()
+
+    # #成员告列表
+    # @seq_task(3)
+    # @task(1)
+    # def test_UserReportList(self):
+    #     self.UserReportList()
+
+    # ##########################################################个人中心###########################################################
+    # #我的慢拍
+    # @task(1)
+    # def test_MyPhoto(self):
+    #     self.MyPhoto()
+
+    # #我的更多慢拍
+    # @task(1)
+    # def test_MyPhotoMore(self):
+    #     self.MyPhotoMore()
+
+    # #慢拍和粉丝统计
+    # @task(1)
+    # def test_MyPhotoStatistics(self):
+    #     self.MyPhotoStatistics()
+
+    # #消息列表
+    # @task(1)
+    # def test_MessageList(self):
+    #     self.MessageList()  
+
+    # #修改我的资料
+    # @task(1)
+    # def test_ModifyMyInfo(self):
+    #     self.ModifyMyInfo()  
+
+    # ##########################################################活动###########################################################
+
+    #活动列表
     @seq_task(2)
     @task(1)
-    def test_UserList(self):
-        self.UserList()
+    def test_ActivityList(self):
+        self.ActivityList()
 
-
-    #同步检测报告
+    #活动详情-慢拍列表
     @seq_task(3)
     @task(1)
-    def test_SyncReport(self):
-        self.SyncReport()
+    def test_ActivityPhotos(self):
+        self.ActivityPhotos()
 
-    #报告列表
+    #活动详情-慢拍投票
     @seq_task(4)
     @task(1)
-    def test_ReportList(self):
-        self.ReportList()
+    def test_PhotoVote(self):
+        self.PhotoVote()
 
-    #成员告列表
-    @seq_task(3)
-    @task(1)
-    def test_UserReportList(self):
-        self.UserReportList()
+    #活动详情-慢拍投票
+    def PhotoVote(self):
+        if len(self.activityphontophontoid) > 0:
+            headers = {"Content-Type":"application/json"}
+            payload = {
+                "lang":"ZH_CN",
+                "token":"slowshot",
+                "version":"1.0.0",
+                "body":{
+                    "memberToken":self.memberToken,
+                    "memberId":self.memberid,
+                    "activityId":self.activityphontoactivityid,
+                    "photoId":self.activityphontophontoid
+                }
+            }
+            r = self.client.post('api/activity/dPhotoVote',data = json.dumps(payload),headers = headers,catch_response = True)
+            if r.status_code == 200:
+                if r.json()["code"] == '200' and r.json()["message"] == '成功': 
+                    r.success()
+                    print(r.json())
+                else:
+                    r.failure(r.json()['message'])
+            else:
+                r.failure("HTTP状态码"+str(r.status_code))
 
-    ##########################################################个人中心###########################################################
-    #我的慢拍
-    @task(1)
-    def test_MyPhoto(self):
-        self.MyPhoto()
+    #活动详情-慢拍列表
+    def ActivityPhotos(self):
+        headers = {"Content-Type":"application/json"}
+        payload = {
+            "lang":"ZH_CN",
+            "token":"slowshot",
+            "version":"1.0.0",
+            "body":{
+                "memberToken":self.memberToken,
+                "memberId":self.memberid,
+                "activityId":self.activityid,
+                "search":"",
+                "currentPage":""
+            }
+        }
+        r = self.client.post('api/activity/qPhotos',data = json.dumps(payload),headers = headers,catch_response = True)
+        if r.status_code == 200:
+            if r.json()["code"] == '200' and r.json()["message"] == '成功': 
+                r.success()
+                self.locust.activityphonto=r.json()["value"]
+                if len(self.locust.activityphonto)>0:
+                    num = random.randint(0,len(self.locust.activityphonto)-1)
+                    self.activityphontoactivityid = self.locust.activityphonto[num]["activityId"]
+                    self.activityphontophontoid = self.locust.activityphonto[num]["photoId"]
+            else:
+                r.failure(r.json()['message'])
+        else:
+            r.failure("HTTP状态码"+str(r.status_code))
 
-    #我的更多慢拍
-    @task(1)
-    def test_MyPhotoMore(self):
-        self.MyPhotoMore()
-
-    #慢拍和粉丝统计
-    @task(1)
-    def test_MyPhotoStatistics(self):
-        self.MyPhotoStatistics()
-
-    #消息列表
-    @task(1)
-    def test_MessageList(self):
-        self.MessageList()  
-
-    #修改我的资料
-    @task(1)
-    def test_ModifyMyInfo(self):
-        self.ModifyMyInfo()  
-
-    ##########################################################活动###########################################################
-
-
-
-
+    #活动列表
+    def ActivityList(self):
+        headers = {"Content-Type":"application/json"}
+        payload = {
+            "lang":"ZH_CN",
+            "token":"slowshot",
+            "version":"1.0.0",
+            "body":{
+                "accessToken":self.locust.accessToken,
+                "memberToken":self.memberToken,
+                "memberId":self.memberid,
+                "search":"",
+                "currentPage":""
+            }
+        }
+        r = self.client.post('api/activity/qList',data = json.dumps(payload),headers = headers,catch_response = True)
+        if r.status_code == 200:
+            if r.json()["code"] == '200' and r.json()["message"] == '成功': 
+                r.success()
+                self.locust.activity=r.json()["value"]  
+                num = random.randint(0,len(self.locust.activity)-1)
+                self.activityid = self.locust.activity[num]["activityId"]
+            else:
+                r.failure(r.json()['message'])
+        else:
+            r.failure("HTTP状态码"+str(r.status_code))
 
     #修改我的资料
     def ModifyMyInfo(self):
@@ -406,7 +502,7 @@ class MyTest(TaskSequence):
             "token":"slowshot",
             "version":"1.0.0",
             "body":{
-                "accessToken":"db20a5ccf7ab11e886e8ec0d9a2fab3e",
+                "accessToken":self.locust.accessToken,
                 "platForm":"ios"
             }
         }
@@ -569,6 +665,8 @@ class BestTestIndexUser(HttpLocust):
     memberinfo = []
     userdatas = []
     user = []
+    activity = []
+    activityphonto = []
     for i in range(len(data)):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
         userdatas.append(data[i])
 
